7d093b2db8f9abcf6c0940bff70396d09d947f1a
Bai Xiao <905333282@qq.com>
2021-10-12 18:06:20 +0800

    create

--- cache/cacheImpl.go
+++ cache/cacheImpl.go
@@ -4,20 +4,12 @@ import "sync"
 
 type GodisCache struct {
 	coreMap map[string]Frame
-	mutex sync.RWMutex
+	mutex   sync.RWMutex
 	Current Overview
 }
 
-
-Set(string, *Frame) error
-Get(string) (*Frame, error)
-Del(string) error
-Overview() Overview
-
-
-func (g *GodisCache) Set(key string, value *Frame)  {
+func (g *GodisCache) Set(key string, value *Frame) {
 	g.mutex.Lock()
 	defer g.mutex.Unlock()
 
-	
 }
